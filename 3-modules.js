148c04301c9d2c8ba265120cb037713e62b116b2
smdekate <smdekate@gmail.com>
2022-11-30 14:55:20 +0530

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,8 @@
+const names = require('./4-names')
+const greet = require('./5-utils')
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+
+// greet(data.items[0])
+// greet(data.items[1])
+// greet(data.singlePerson.name)
---
+++ 4-names.js
@@ -0,0 +1,9 @@
+
+// share
+const john = "john"
+const peter = 'Peter'
+
+// local
+const SECRET = 'SUPER SECRET'
+
+module.exports = {john, peter}
